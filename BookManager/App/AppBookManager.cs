d89c85614468f0dafc942dffeec2dfe7fd4fb71b
Administrator <Administrator@PC269>
2017-12-15 10:19:08 +0700

    appbookmanager

--- BookManager/App/AppBookManager.cs
+++ BookManager/App/AppBookManager.cs
@@ -8,5 +8,31 @@ namespace BookManager.App
 {
     class AppBookManager
     {
+        private BookList booklist = new BookList();
+        public void run()
+        {
+            int chon ;
+            do
+            {
+                Console.WriteLine("1.Nhap danh sach sach");
+                Console.WriteLine("2.Xuat danh sach sach");
+                Console.WriteLine("3.Thoat");
+                Console.WriteLine("Chon:");
+                chon = int.Parse(Console.ReadLine());
+
+                switch(chon){
+                    case 1:
+                        Console.WriteLine("Nhap so luong sach:");
+                        int sl = int.Parse(Console.ReadLine());
+                        booklist.input(sl);
+                        break;
+                    case 2:
+                        booklist.output();
+                        break;
+                    default:
+                        break;
+                }
+            } while (true);
+        }
     }
 }
--- BookManager/Program.cs
+++ BookManager/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookManager.App;
 
 namespace BookManager
 {
@@ -10,7 +11,8 @@ namespace BookManager
     {
         static void Main(string[] args)
         {
-            //cc
+            AppBookManager app = new AppBookManager();
+            app.run();
         }
     }
 }
