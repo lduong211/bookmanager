a64161545916942ec0f2b86c627cc6cfdc5e7ce0
Administrator <Administrator@PC269>
2017-12-15 09:00:57 +0700

    interface

---
+++ BookManager/Interface/IBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Entities
+{
+    interface IBook
+    {
+        string this[int index] { get;set; }
+
+        string Title { get; set; }
+
+        string Author { get; set; }
+
+        string Publisher { get; set; }
+
+        string ISBN { get; set; }
+
+        string Year { get; set; }
+    }
+    
+}
